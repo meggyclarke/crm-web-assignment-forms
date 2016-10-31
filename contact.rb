e0d5a1916a3ba286961205352e2c8378360502e0
Meg Clarke <meggyclarke@gmail.com>
2016-10-31 13:56:50 -0400

    Adding Active Record

--- contact.rb
+++ contact.rb
@@ -1,4 +1,8 @@
-require 'sinatra'
+gem 'activerecord', '=4.2.7'
+require 'active_record'
+require 'mini_record'
+
+ActiveRecord::Base.establish_connection(adapter:'sqlite3', database: 'crm-web.sqlite3')
 
 class Contact
      attr_accessor :id
