e33bbc27334e1237630566566d4f88fe7ef4a562
Meg Clarke <meggyclarke@gmail.com>
2016-10-31 10:15:49 -0400

    CRM new version with Forms working

---
+++ contact.rb
@@ -0,0 +1,87 @@
+require 'sinatra'
+
+class Contact
+     attr_accessor :id
+
+    @@contacts = []
+    @@id = 1
+
+    attr_accessor :first_name, :last_name, :email, :note
+
+  def initialize(first_name, last_name, email, note)
+    @first_name = first_name
+    @last_name = last_name
+    @email = email
+    @note = note
+    @id = @@id
+    @@id += 1
+  end
+
+  def self.create(first_name, last_name, email, note)
+    new_contact = Contact.new(first_name, last_name, email, note)
+    @@contacts << new_contact
+    return new_contact
+  end
+
+    def self.all
+        @@contacts
+    end
+
+  def self.find(id)
+        @@contacts.find do |contact|
+            contact.id == id.to_i
+        end
+  end
+
+   def update(key,new_value)
+        if key == "first_name"
+        self.first_name = new_value
+        elsif key == "last_name"
+        self.last_name = new_value
+        elsif key == "email"
+        self.email = new_value
+        else key == "note"
+        self.note = new_value
+        end
+    end
+
+  def self.find_by(thing1, thing2)
+       @@contacts.each do |contact|
+           if thing1 == "first_name" && thing2 == contact.first_name
+               return contact
+            elsif thing1 == "last_name" && thing2 == contact.last_name
+                return contact
+            elsif thing1 == "email" && thing2 == contact.email
+                return contact
+            elsif thing1 == "note" && thing2 == contact.note
+                return contact
+            else
+                puts "That is not a searchable term."
+            end
+        end
+  end
+
+  def delete
+      @@contacts.delete(self)
+  end
+
+  def self.delete_all
+
+    @@contacts.clear
+  end
+
+  def full_name
+      full_name = "#{first_name} #{last_name}"
+  end
+
+ rainbow = ["colour_a", "colour_b", "colour_c", "colour_d"]
+
+end
+
+Contact.create("Meg", "Clarke", "meg@meg.com", "nutmeg")
+Contact.create("Steph", "Stephy", "steph@steph", "crazy")
+Contact.create("Marty", "McFly", "marty@mcfly.com", "Cool Shoes")
+Contact.create("George", "McFly", "george@mcfly.com", "Sci-Fi Author")
+Contact.create("Lorraine", "McFly", "lorraine@mcfly.com", "Shock hop champion")
+Contact.create("Biff", "Tannen", "biff@tannen.com", "Professional Looser")
+Contact.create("Doc", "Brown", "doc@brown.com", "88 kilowats")
---
+++ crm.rb
@@ -0,0 +1,72 @@
+require 'sinatra'
+require_relative 'contact.rb'
+
+get '/' do
+    @crm_app_name = "Meggy's CRM"
+    erb :index
+end
+
+get '/contacts' do
+    erb :contacts
+end
+
+get '/new_contact' do
+    erb :new_contact
+end
+
+get '/show_contacts' do
+    erb :show_contacts
+end
+get '/about' do
+    erb :about
+end
+
+get '/edit_contact' do
+    erb :edit_contact
+end
+
+post '/contacts' do
+    Contact.create(params[:first_name], params[:last_name], params[:email], params[:note])
+    redirect to(:contacts)
+end
+
+get '/contacts/:id' do
+    @contact = Contact.find(params[:id].to_i)
+        erb :show_contacts
+end
+
+get '/contacts/:id/edit' do
+    @contact = Contact.find(params[:id].to_i)
+ if @contact
+   erb :edit_contact
+ else
+   raise Sinatra::NotFound
+ end
+end
+
+put '/contacts/:id' do
+  @contact = Contact.find(params[:id].to_i)
+  if @contact
+    @contact.first_name = params[:first_name]
+    @contact.last_name = params[:last_name]
+    @contact.email = params[:email]
+    @contact.note = params[:note]
+
+    redirect to('/contacts')
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+delete '/contacts/:id' do
+  @contact = Contact.find(params[:id].to_i)
+  if @contact
+    @contact.delete
+    redirect to('/contacts')
+  else
+    raise Sinatra::NotFound
+  end
+end
+# class Time
+#     Time.new(26, 10,2016).to_date
+# end
